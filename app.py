5821b3ae9801e1b1f69adeff9d6481195f1eff97
Shenbaga Balaji <214503138+shenal19@users.noreply.github.com>
2025-07-28 19:45:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     result = {}
+    score = None  # Transparency score initialized
+
     if request.method == "POST":
         text = ""
 
@@ -17,9 +19,11 @@ def home():
             text = request.form.get("tnc_text")
 
         if text:
-            result = get_relevant_clauses(text)
+            output = get_relevant_clauses(text)
+            result = output["clauses"]
+            score = output["score"]
 
-    return render_template("index.html", result=result)
+    return render_template("index.html", result=result, score=score)
 
 if __name__ == "__main__":
     app.run(debug=True)
